151fd4f2b07518db604be9e18384a41631bd2a31
dolf <dolf@localhost.localdomain>
2025-06-26 23:19:03 +0200

    added elogind as a dependency

--- install_dep.sh
+++ install_dep.sh
@@ -1,14 +1,16 @@
-#!/user/bin/bash
+#!/usr/bin/bash
 
 sudo xbps-install -u xbps
 sudo xbps-install -Su
 
-sudo xbps-install dbus polkit xorg libXft-devel libXinerama-devel base-devel libX11-devel nitrogen picom NetworkManager
+sudo xbps-install dbus polkit elogind xorg libXft-devel libXinerama-devel base-devel libX11-devel nitrogen NetworkManager
 
+sudo touch /etc/sv/acpid/down
 sudo touch /etc/sv/dhcpcd/down
 sudo touch /etc/sv/wpa_supplicant/down
 
+sudo sv down acpid
 sudo sv down dhcpcd
 sudo sv down wpa_supplicant
 
-sudo ln -sv /etc/sv/{dbus,polkitd,NetworkManager} /var/service
+sudo ln -sv /etc/sv/{dbus,polkitd,elogind,NetworkManager} /var/service
